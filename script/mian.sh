c655dc71de00d589d02347cf249a814696acc42c
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-13 14:23:30 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
 # 下载规则
-curl -o i-1.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
-curl -o i-2.txt https://filters.adtidy.org/extension/ublock/filters/224.txt
-curl -o i-3.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
-curl -o i-4.txt https://www.i-dont-care-about-cookies.eu/abp/
-curl -o i-5.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
-curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock.txt
-curl -o i-7.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt
-curl -o i-8.txt https://cdn.jsdelivr.net/gh/uBlockOrigin/uAssetsCDN@main/filters/filters.txt
-curl -o i-9.txt https://cdn.statically.io/gh/uBlockOrigin/uAssetsCDN/main/filters/resource-abuse.txt
-curl -o i-10.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
-curl -o i-11.txt https://raw.githubusercontents.com/timlu85/AdGuard-Home_Youtube-Adfilter/master/Youtube-Adfilter-Web.txt
-curl -o i-12.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/dns.txt
-curl -o i-13.txt https://filters.adtidy.org/extension/ublock/filters/11.txt
-curl -o i-14.txt https://raw.githubusercontent.com/zsakvo/AdGuard-Custom-Rule/master/rule/zhihu.txt
-curl -o i-15.txt https://raw.githubusercontents.com/91ajames/ublock-filters-ulist-youtube/main/blocklist.txt
+curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock.txt
+curl -o i-2.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
+curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt
+curl -o i-4.txt https://filters.adtidy.org/extension/ublock/filters/224.txt
+curl -o i-5.txt https://www.i-dont-care-about-cookies.eu/abp/
+curl -o i-6.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
+curl -o i-7.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+curl -o i-8.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt
+curl -o i-9.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/dns.txt
+curl -o i-10.txt https://raw.githubusercontents.com/timlu85/AdGuard-Home_Youtube-Adfilter/master/Youtube-Adfilter-Web.txt
+curl -o i-11.txt https://filters.adtidy.org/extension/ublock/filters/11.txt
+curl -o i-12.txt https://raw.githubusercontent.com/zsakvo/AdGuard-Custom-Rule/master/rule/zhihu.txt
+curl -o i-13.txt https://raw.githubusercontents.com/91ajames/ublock-filters-ulist-youtube/main/blocklist.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
@@ -30,7 +28,7 @@ num=`cat i-tmp.txt | wc -l`
 # 添加标题和时间
 echo "[Adblock Plus 2.0]" >> i-tpdate.txt
 echo "! Title: AdGuard Rules" >> i-tpdate.txt
-echo "! Description: 该规则合并自各位大神规则以及补充的规则" >> i-tpdate.txt
+echo "! Description: 该规则合并自各位大神规则" >> i-tpdate.txt
 echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> i-tpdate.txt
 echo "! Total count: $num" >> i-tpdate.txt
 cat i-tpdate.txt i-tmp.txt > ad.txt
