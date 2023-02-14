e4875fdee1e925f02e38446da15229ad94b79ca1
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-14 23:19:35 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,11 +4,8 @@
 curl -o i-1.txt https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/abp_light.txt
 curl -o i-2.txt https://code.gitlink.org.cn/damengzhu/abpmerge/raw/branch/main/abpmerge.txt
 curl -o i-3.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
-curl -o i-4.txt https://code.gitlink.org.cn/api/v1/repos/keytoolazy/adblock/raw/adblock_lite
-curl -o i-5.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/domain.txt
-curl -o i-6.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/regex.txt
-curl -o i-7.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/adguardhome.txt
-curl -o i-8.txt https://raw.githubusercontent.com/samyansan/Ad-hosts/master/adguard
+curl -o i-4.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/domain.txt
+curl -o i-5.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/regex.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
