0751fb52551ec9564f43320f277f5df289b51fd5
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-15 16:47:32 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -2,10 +2,11 @@
 
 # 下载规则
 curl -o i-1.txt https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/abp_light.txt
-curl -o i-2.txt https://code.gitlink.org.cn/damengzhu/abpmerge/raw/branch/main/abpmerge.txt
+curl -o i-2.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
 curl -o i-3.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
-curl -o i-4.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/domain.txt
+curl -o i-4.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_38.txt
 curl -o i-5.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/regex.txt
+curl -o i-6.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_24.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
