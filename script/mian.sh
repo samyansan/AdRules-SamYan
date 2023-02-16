32cceb834a9c1dcfd6f81039459a349329648972
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-16 12:18:05 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # 下载规则
-curl -o i-1.txt https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/abp_light.txt
-curl -o i-2.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
-curl -o i-3.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
+curl -o i-1.txt https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_basic.txt
+curl -o i-2.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
+curl -o i-3.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
 curl -o i-4.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
-curl -o i-5.txt http://sub.adtchrome.com/adt-chinalist-easylist.txt
-curl -o i-6.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
+curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
+curl -o i-6.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/domain.txt
 curl -o i-7.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/regex.txt
-curl -o i-8.txt https://raw.githubusercontent.com/neodevpro/neodevhost/master/adblocker
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
