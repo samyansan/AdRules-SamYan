d920a16e33f379be4e2965de1b6a44431ec3d5c6
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-18 12:47:22 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,16 +4,12 @@
 curl -o i-1.txt https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_basic.txt
 curl -o i-2.txt https://code.gitlink.org.cn/api/v1/repos/keytoolazy/adblock/raw/adblock_lite
 curl -o i-3.txt https://code.gitlink.org.cn/damengzhu/abpmerge/raw/branch/main/abpmerge.txt
-curl -o i-4.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_38.txt
-curl -o i-5.txt https://raw.githubusercontent.com/samyansan/Ad-hosts/master/adguard
-curl -o i-6.txt https://raw.githubusercontent.com/Crystal-RainSlide/AdditionalFiltersCN/master/CN.txt
-curl -o i-7.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/adguardhome.txt
-curl -o i-8.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
-curl -o i-9.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_24.txt
+curl -o i-4.txt https://raw.githubusercontent.com/Crystal-RainSlide/AdditionalFiltersCN/master/CN.txt
+curl -o i-5.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
-cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v '^@' | grep -v '^@ ' | grep -v '^###############################################################################################' | grep -v '^############################################################################################### ' | grep -v '^###################################' | grep -v '^################################### ' > i-tmpp.txt
+cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v '^@' | grep -v '^@ ' > i-tmpp.txt
 sort -n i-tmpp.txt | uniq > i-tmp.txt
 
 python rule.py i-tmp.txt
