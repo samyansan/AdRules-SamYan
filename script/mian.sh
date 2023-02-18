436900798c1af05b7422d7c94caa59d44e8ba2de
samyansan <41437406+samyansan@users.noreply.github.com>
2023-02-18 13:20:10 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -6,6 +6,11 @@ curl -o i-2.txt https://code.gitlink.org.cn/api/v1/repos/keytoolazy/adblock/raw/
 curl -o i-3.txt https://code.gitlink.org.cn/damengzhu/abpmerge/raw/branch/main/abpmerge.txt
 curl -o i-4.txt https://raw.githubusercontent.com/Crystal-RainSlide/AdditionalFiltersCN/master/CN.txt
 curl -o i-5.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
+curl -o i-6.txt https://raw.githubusercontent.com/hacamer/Adblist/master/dns.txt
+curl -o i-7.txt https://raw.githubusercontent.com/hacamer/Adblist/master/adp.txt
+curl -o i-8.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt
+curl -o i-9.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_29.txt
+curl -o i-10.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
