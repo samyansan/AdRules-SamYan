4c79552a8c76396ef9f08a56143386cb69fac18b
samyansan <41437406+samyansan@users.noreply.github.com>
2023-03-03 20:41:19 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -12,7 +12,7 @@ curl -o i-8.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
-cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v '^@' | grep -v '^ @' > i-tmpp.txt
+cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v '^@' | grep -v '$ghide' > i-tmpp.txt
 sort -n i-tmpp.txt | uniq > i-tmp.txt
 
 python rule.py i-tmp.txt
