0bf12bad66fa7ef07fe8275d75c5e440ca536391
samyansan <41437406+samyansan@users.noreply.github.com>
2023-03-12 16:06:28 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -3,7 +3,7 @@
 # 下载规则
 curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
 curl -o i-2.txt https://code.gitlink.org.cn/api/v1/repos/keytoolazy/adblock/raw/adblock_lite
-curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
+curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt
 curl -o i-4.txt https://raw.githubusercontent.com/samyansan/AdHosts/master/adguard
 curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
 curl -o i-6.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
