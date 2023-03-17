c940a4e6037e9997d99142d13e295d863e3e3cdc
samyansan <41437406+samyansan@users.noreply.github.com>
2023-03-17 21:01:04 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -3,12 +3,9 @@
 # 下载规则
 curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt
 curl -o i-2.txt https://code.gitlink.org.cn/api/v1/repos/keytoolazy/adblock/raw/adblock_lite
-curl -o i-3.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt
-curl -o i-4.txt https://raw.githubusercontent.com/samyansan/AdHosts/master/adguard
-curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
-curl -o i-6.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
-curl -o i-7.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt
-curl -o i-8.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
+curl -o i-3.txt https://raw.githubusercontent.com/samyansan/AdGuardRules/main/adguard.txt
+curl -o i-4.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
+curl -o i-5.txt https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
