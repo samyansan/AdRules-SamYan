8294bc5e9c2d647d98f96833b9d81263f8e6e412
samyansan <41437406+samyansan@users.noreply.github.com>
2023-12-26 18:04:43 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # 下载规则
-curl -o i-1.txt https://www.kbsml.com/wp-content/uploads/adblock/adguard/adg-kall.txt
-curl -o i-2.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock.txt
-curl -o i-3.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
-curl -o i-4.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt
-curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
-curl -o i-6.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
+curl -o i-1.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock.txt
+curl -o i-2.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
+curl -o i-3.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt
+curl -o i-4.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/dns-rules.txt
+curl -o i-5.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mod/rules/adblock-rules.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
