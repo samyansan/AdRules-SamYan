02a7f6cb217e3eb86623fd4c00690a056bdcb5e8
samyansan <41437406+samyansan@users.noreply.github.com>
2025-11-07 11:10:56 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -6,7 +6,7 @@ curl -o i-2.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
-cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^||||' | grep -v '^|||| ' | grep -v '^|||' | grep -v '^||| ' | grep -v '^0.0.0.0' | grep -v '^0.0.0.0 ' | grep -v '^127.0.0.1' | grep -v '^127.0.0.1 ' | grep -v '^## ' | grep -v '^## ' | grep -v '^\[' | grep -v '^\【' > i-tmpp.txt
+cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^||||' | grep -v '^|||| ' | grep -v '^|||' | grep -v '^||| ' | grep -v '^0.0.0.0' | grep -v '^0.0.0.0 ' | grep -v '^127.0.0.1' | grep -v '^127.0.0.1 ' | grep -v '^##' | grep -v '^## ' | grep -v '^\[' | grep -v '^\【' | grep -v '#' | grep -v '##' | grep -v '###' > i-tmpp.txt
 sort -n i-tmpp.txt | uniq > i-tmp.txt
 
 python rule.py i-tmp.txt
